16cf1d4355b095515506ad22a3d32bb9086c29e3
DeakUchir <yogimal165@iucake.com>
2023-06-24 09:15:25 +0000

    ++

--- UInstaller.h
+++ UInstaller.h
@@ -14,9 +14,11 @@ class fRead
 public:
 	
 	int count = 0;
+	
 	string *readF(string file_n)
 	{
 		string *str_buff = new string[200];
+		
 		ifstream file;
 		file.open(file_n);
 		
@@ -45,6 +47,7 @@ public:
 	{
 		string cmd_s = "apt-get install " + prut;
 		char cmd_c[1024];
+		
 		strcpy(cmd_c, cmd_s.c_str());
 		system(cmd_c);
 	}
@@ -55,22 +58,14 @@ public:
 		string *arr = new string[200];
 		
 		arr = readF(file_name);
-
 		lenA = end(*arr)-begin(*arr);
-		
 
 		update();
-
-
 
 		for (int i=0; i<lenA; i++)
 			install(arr[i]);
-		
 
 		update();
-
-		
-
 	}
 };
 
